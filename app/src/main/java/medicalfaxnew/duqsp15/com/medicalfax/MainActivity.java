e5ee1cb96ac1009193b14269f5072f9ae11854c6
Kinardi Isnata <isnatak@duq.edu>
2015-02-16 11:49:48 -0500

    I added Up, Down, and Dictate button with their strings fields. They don't have the associated methods yet. I have written down Up and Down method for the buttons (including import Intent and View library), but they are not functioning yet. Also, I embedded a Relative Layout for the buttons, inside the text boxes Linear Layout.

--- app/src/main/java/medicalfaxnew/duqsp15/com/medicalfax/MainActivity.java
+++ app/src/main/java/medicalfaxnew/duqsp15/com/medicalfax/MainActivity.java
@@ -1,9 +1,11 @@
 package medicalfaxnew.duqsp15.com.medicalfax;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +37,25 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    /** This method is called when Up Button is clicked
+     *  It causes the cursor to move up once to the text field above the current position
+     *  Created by: Kinardi Isnata, February 16 2015
+     */
+    public void up(View view)
+    {
+       // Intent intent = new Intent(this, );
+       // If it is not at the most top position, then move cursors up from current position
+       // EditText.setSelection(int);
+    }
+    /** This method is called when Down Button is clicked
+     *  It causes the cursor to move down once to the text field below the current position
+     *  Created by: Kinardi Isnata, February 16 2015
+     */
+    public void down(View view)
+    {
+        // Intent intent = new Intent(this, );
+        // If it is not at the most bottom position, then move cursors down from current position
+        // EditText.setSelection(int);
     }
 }
