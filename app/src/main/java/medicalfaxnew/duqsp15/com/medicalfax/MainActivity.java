348642517bb0688359d134d510b9c96a7ccf3cd4
Kinardi Isnata <isnatak@duq.edu>
2015-02-18 09:52:21 -0500

    The Dictate button will store a fixed (for now, test purpose) String into a selected text box. The only textbox that can be selected is the Patient ID. android:onClick="setSelection" needs to be added on every editText. Potential Bug: you need to double tap the edittext to select it.

--- app/src/main/java/medicalfaxnew/duqsp15/com/medicalfax/MainActivity.java
+++ app/src/main/java/medicalfaxnew/duqsp15/com/medicalfax/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private int SELECTED = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +47,10 @@ public class MainActivity extends ActionBarActivity {
      */
     public void up(View view)
     {
-       // Intent intent = new Intent(this, );
-       // If it is not at the most top position, then move cursors up from current position
-       // EditText.setSelection(int);
+
+        // setFocus by decrement view.getId()
+        // If it is not at the most top position, then move cursors up from current position
+        // EditText.setSelection(int);
     }
     /** This method is called when Down Button is clicked
      *  It causes the cursor to move down once to the text field below the current position
@@ -54,8 +58,36 @@ public class MainActivity extends ActionBarActivity {
      */
     public void down(View view)
     {
-        // Intent intent = new Intent(this, );
+
+        // setFocus by increment view.getId()
         // If it is not at the most bottom position, then move cursors down from current position
         // EditText.setSelection(int);
     }
+
+    /**
+     * This method is called when the dictate button is pressed
+     * @param view is the dictate button
+     */
+
+    public void setSelection(View view)
+    {
+        SELECTED = view.getId();
+        System.out.println(SELECTED);
+    }
+
+    /**
+     * Set the selected EditText with the transcribed text
+     * WARNING: For the Edittext to be selected, it needs to be double clicked
+     * @param view
+     */
+    public void dictates(View view)
+    {
+        String test = "HeLLLooo"; //for test
+        if(SELECTED >= 0) {
+            EditText textBox = (EditText) findViewById(SELECTED);
+            textBox.setText(test);
+        }
+        SELECTED = -1;
+    }
+
 }
