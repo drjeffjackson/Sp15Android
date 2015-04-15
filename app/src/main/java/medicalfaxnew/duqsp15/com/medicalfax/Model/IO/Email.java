65c4e84d69e673282bcb7e96f295ce1c389834d9
Phil Custer <custer55@gmail.com>
2015-04-15 14:46:08 -0400

    Added option for recipient and CC's to be specified and pre-filled with the sendEmail method.

--- app/src/main/java/medicalfaxnew/duqsp15/com/medicalfax/Model/IO/Email.java
+++ app/src/main/java/medicalfaxnew/duqsp15/com/medicalfax/Model/IO/Email.java
@@ -13,6 +13,11 @@ public class Email {
      * created by Xun Yang, on 3/30/2015,working with Joel and Phil
      */
 
+    /*
+     * 4/15/15 - Phil C, Added functionality for specifying recipient
+     * and CC's.  Also added comments for clarity.
+     */
+
     public Activity activity;
     ModelInterface modelE;
     public Email(Activity ac, ModelInterface model)
@@ -20,7 +25,7 @@ public class Email {
         modelE=model;
         activity=ac;
     }
-    public void sendEmail(String body, Uri u){
+    public void sendEmail(String[] recipients, String[] carbon, String body, Uri u){
         Log.w("=============",u.toString());
         /* ACTION_SEND indicates that one attachment will be sent. */
         Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
@@ -29,9 +34,17 @@ public class Email {
         /* Set flags to allow the Uri to be edited. */
         emailIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         emailIntent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        /* If recipients are specified then add them to the intent. */
+        if(recipients != null) {
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, recipients);
+        }
+        /* If CC's are specified then add them to the intent. */
+        if(carbon != null){
+            emailIntent.putExtra(Intent.EXTRA_CC, carbon);
+        }
         emailIntent.setType("text/plain");
         /* Passes the subject for the email. */
-        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "patient information");
+        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Transferring Patient Information");
         /* Passes the body of the email. */
         emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, body);
         /* Passes the attachment as a Uri. */
